47b6d01e1d194aefaa1f68cb6da75fa1c083f520
andrew644 <andrew644@users.noreply.github.com>
2026-02-09 22:45:13 -0500

    Add end-to-end tests

--- Tests/EndToEndTests/TestAll.cs
+++ Tests/EndToEndTests/TestAll.cs
@@ -26,10 +26,36 @@ public class TestAll
     }
     
     [Theory]
-    [InlineData("function")]
-    public void RunTests(string name)
+    [InlineData("function", 0)]
+    [InlineData("if", 0)]
+    [InlineData("fizzbuzz", 40)] //TODO void main returns 40. Should we force this to be 0?
+    [InlineData("simple1", 42)]
+    [InlineData("simple2", 43)]
+    public void RunTests(string name, int expectedExitCode)
     {
-        var source = $"../../../../dux_test_src/{name}.dux";
-        var expected = File.ReadAllText($"../../../../dux_test_src/{name}.expected");
+        const string baseDir = "../../../../";
+        const string testDir = $"{baseDir}dux_test_src/";
+        const string outDir = $"{testDir}out/";
+        var irPath = $"{outDir}{name}.ll";
+        var binPath = $"{outDir}{name}";
+        var srcPath = $"{testDir}{name}.dux";
+        
+        RunProcess("mkdir", $"-p {outDir}"); // make sure out directory exists
+
+        var (exitCode, _, stdErr) = RunProcess($"{baseDir}Compiler/bin/Debug/net10.0/Compiler", $"{srcPath} {irPath}");
+        Assert.Equal("", stdErr);
+        Assert.Equal(0, exitCode);
+        
+        var (clangExitCode, clangStdOut, clangStdErr) = RunProcess($"clang", $"-o {binPath} {irPath}");
+        Assert.Equal("", clangStdErr);
+        Assert.Equal("", clangStdOut);
+        Assert.Equal(0, clangExitCode);
+        
+        var (binExitCode, binStdOut, binStdErr) = RunProcess($"{binPath}", "");
+        Assert.Equal("", binStdErr);
+        
+        var expected = File.ReadAllText($"{testDir}{name}.exp");
+        Assert.Equal(expected, binStdOut);
+        Assert.Equal(expectedExitCode, binExitCode);
     }
 }
